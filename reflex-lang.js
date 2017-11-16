7e64133771914f224f9542c3d3487caccc3b83f7
dou4cc <dou4cc@live.cn>
2017-11-16 17:51:42 +0800

    .

--- reflex-lang.js
+++ reflex-lang.js
@@ -31,19 +31,23 @@ const reflex = free => {
 	return f([]);
 };
 
-const flatten = list => {
+const flatten = (listify, ...list) => {
+	listify = listify || (a => a instanceof Array ? a : null);
 	const begin = Symbol();
 	const end = Symbol();
-	const f = list => [].concat(...list.map(a => a instanceof Array ? f([begin, ...a, end]) : [a]));
-	return [begin, end, f(list)];
+	const used = new Set;
+	const holes = new Set;
+	const f = list => [].concat(...list.map(a => {
+		if(used.has(a) && holes.add(a) || !listify(a)) return [a];
+		used.add(a);
+		return [begin, ...f(listify(a)), end];
+	}));
+	return [holes, begin, end, ...f(list)];
 };
 
-const unflatten = (begin, end, [...list]) => {
+const unflatten = (unlistify, begin, end, ...list) => {
 	let pos;
-	while((pos = list.indexOf(end)) >= 0){
-		const list1 = [];
-		list1.push(...list.splice(pos - (pos -= list.lastIndexOf(begin, pos)), pos + 1, list1).slice(1, -1));
-	}
+	while((pos = list.lastIndexOf(begin)) >= 0) list.splice(pos, 0, (unlistify || (a => a))(list.splice(pos, list.indexOf(end, pos) - pos + 1).slice(1, -1)));
 	return list;
 };
 
@@ -76,69 +80,95 @@ const multi_key_map = () => {
 	};
 };
 
-const vm = () => {
-	const flatten1 = list => {
-		const [begin, end] = [, , list] = flatten(list);
-		return list.map(a => a === begin ? begin0 : a === end ? end0 : a);
-	};
-	const unflatten1 = list => unflatten(begin0, end0, list);
-	const begin0 = Symbol();
-	const end0 = Symbol();
-	const reflex0 = reflex();
-	const handles = multi_key_map();
-	reflex0.on("on", (...list) => {
-		if(handles.get("on", ...list)) return;
-		const trigger = unflatten1(list);
-		const pattern = trigger[0];
-		const list1 = flatten1(pattern);
-		const path = list1.slice(0, list1.concat(0).indexOf(0));
-		handles.set("on", ...list, reflex0.on(...path.map(a => typeof a === "number" ? a - 1 : a), (...list) => {
-			const match = (pattern, list) => {
-				if(pattern === 0) return args.push(list);
-				if(list.length > 1) return;
-				if(pattern instanceof Array && list[0] instanceof Array) return pattern.length <= list[0].length && list[0].splice(0, pattern.length - 1).map(a => [a]).concat(list).every((a, i) => match(pattern[i], a));
-				return (typeof pattern === "number" ? pattern - 1 : pattern) === list[0];
-			};
-			const args = [, ];
-			if(match(pattern, [unflatten1(path.concat(list))])) unflatten1([].concat(...flatten1(trigger.slice(1)).map(a => {
-				if(typeof a !== "number") return [a];
-				if(a === 0) return [trigger];
-				if(a < args.length) return args[a];
-				return [a - args.length];
-			}))).forEach(signal => reflex0.emit(...flatten1(signal)));
-		}));
-	});
-	reflex0.on("off", (...list) => (handles.get("on", ...list) || (() => {}))());
-	return {
-		emit: (...signals) => signals.forEach(signal => reflex0.emit(...flatten1(signal))),
-		on: (...path) => {
-			const listener = path.pop();
-			return reflex0.on(...path, (...list) => listener(...unflatten1(list)));
-		},
+const same_list = (a, b) => a && a.length === b.length && a.every((a, i) => a === b[i] || Object.is(a, b[i]));
+
+const cache = f => {
+	let args;
+	let result;
+	const results = new WeakMap;
+	return (...args1) => {
+		if(same_list(args, args1)) return result;
+		if(args1.length === 1){
+			let result1 = results.get(args1[0]);
+			if(result1) return result1[0];
+			result1 = f(args1[0]);
+			try{
+				results.set(args1[0], [result1]);
+				return result1;
+			}catch(error){}
+			result = result1;
+		}else{
+			result = f(...args1);
+		}
+		args = args1;
+		return result;
 	};
 };
 
-const str2list = source => {
+const is_char = a => typeof a === "string" && a.length === 1;
+
+const vm = () => {
+	const listify = cache(a => a instanceof Array ? a : typeof a === "string" ? Array.from(a) : null);
+	const flatten1 = (...list) => {
+		const [, begin1, end1] = [, , , ...list] = flatten(listify, ...list);
+		return list.map(a => a === begin1 ? begin : a === end1 ? end : a);
+	};
+	const unflatten1 = (...list) => unflatten(list => list.every(is_char) ? list.join("") : list, begin, end, ...list);
+	const on = (path, listener) => reflex0.on(...flatten1(path).slice(0, ...listify(path) ? [-1] : []), (...list) => listener(...unflatten1(...list.slice(0, -1))));
+	const emit = (...signals) => signals.forEach(signal => reflex0.emit(...flatten1(signal)));
+	const begin = Symbol();
+	const end = Symbol();
+	const reflex0 = reflex();
+	const handles = multi_key_map();
+	reflex0.on(begin, ...flatten1("on"), (...list) => {
+		if(handles.get(begin, ...flatten1("on"), ...list)) return;
+		const [pattern, ...signals] = unflatten1(...list.slice(0, -1));
+		const list1 = flatten1(...pattern);
+		const path = list1.slice(0, list1.concat(0).indexOf(0));
+		handles.set(begin, ...flatten1("on"), ...list, reflex0.on(...path.map(a => typeof a === "number" ? a - 1 : a), (...list) => {
+			const match = (pattern, ...signals) => {
+				if(pattern === 0) return args.push(signals);
+				if(signals.length > 1) return;
+				if(!listify(pattern) || !listify(signals[0])) return same_list([typeof pattern === "number" ? pattern - 1 : pattern], signals);
+				[pattern, signals] = [pattern, signals].map(listify);
+				return pattern.length <= signals[0].length && signals[0].splice(0, pattern.length - 1).map(a => [a]).concat(signals).every((a, i) => match(pattern[i], ...a));
+			};
+			const args = [[pattern, ...signals]];
+			if(match(pattern, unflatten1(...path.concat(list)))) emit(...unflatten1([].concat(...flatten1(...signals).map(a => {
+				if(typeof a !== "number") return [a];
+				return a < args.length ? args[a] : [a - args.length];
+			}))));
+		}));
+	});
+	reflex0.on(begin, ...flatten1("off"), (...list) => (handles.get(begin, ...flatten1("on"), ...list) || (() => {}))());
+	return {
+		on,
+		emit,
+		exec: code => emit(...code2signals(code)),
+	};
+};
+
+const code2ast = source => {
 	source = source.replace(/\r\n?/gu, "\n");
-	const words = source.match(/`(?:\\`|[^`])*`|[[\]]|(?:(?!\])\S)+/gu);
-	return unflatten("[", "]", words.map(a => {
+	const words = source.match(/`(?:\\`|[^`])*`|[[\]]|(?:(?!\]|;)\S)+|;.*/gu);
+	return unflatten(null, "[", "]", ...words.filter(a => !a.startsWith(";")).map(a => {
 		if(/^[[\]]$/u.test(a)) return a;
 		if(/^`.*`$/u.test(a)) return {flag: "`", content: a.slice(1, -1).replace(/\\(\\*`)/gu, "$1")};
 		return {flag: "", content: a};
 	}));
 };
 
-const list2signals = source => {
+const ast2signals = source => {
 };
 
-const str2signals = source => list2signals(str2list(source));
+const code2signals = source => ast2signals(code2ast(source));
 
 /*test*
 
 var vm0 = vm();
-vm0.on("+", (a, b, ...rest) => rest.length || vm0.emit(["+", a, b, (+a + +b).toString()]));
-vm0.on("echo", console.log);
-vm0.emit(["on", ["+", 0, 0, 0], ["echo", 1, "+", 2, "=", 3]]);
+vm0.on(["+"], (a, b, ...rest) => rest.length || vm0.emit(["+", a, b, (+a + +b).toString()]));
+vm0.on(["log"], console.log);
+vm0.emit(["on", ["+", 0, 0, 0], ["log", 1, "+", 2, "=", 3]]);
 vm0.emit(["+", "1", "1"]);
 
 //*/
