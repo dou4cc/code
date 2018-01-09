080a6273342b79dff01e44e24d442fe23c1382ad
dou4cc <dou4cc@live.cn>
2018-01-09 12:37:10 +0800

    .

--- archiveis.js
+++ archiveis.js
@@ -2,17 +2,11 @@
 
 {
 	const f = url => {
-		const web_reg = /^https?:/u;
-		url = url || prompt("URL:", web_reg.test(document.location) ? document.location : "");
 		if(url === null) return;
-		const base = document.createElement("base");
-		base.href = "https://"
 		document.head.appendChild(base);
-		const iframe = document.createElement("iframe");
 		iframe.src = url;
 		url = iframe.src;
 		base.remove();
-		const archiveis_reg = /^https?:\/\/archive\.(?:is|today)\//u;
 		url = url.replace(archiveis_reg, "https://archive.is/");
 		if(url !== (url = url.replace(/^https:\/\/archive\.is\/o\/(?:[A-Za-z0-9]{4,}\/)?/u, ""))) return f(url);
 		url = archiveis_reg.test(url) ? url : "https://archive.is/?run=1&url=" + encodeURIComponent("https://4r.gitlab.io/#" + url);
@@ -22,4 +16,10 @@
 			document.location = url;
 		}
 	};
+	const web_reg = /^https?:/u;
+	const base = document.createElement("base");
+	base.href = "https://"
+	const iframe = document.createElement("iframe");
+	const archiveis_reg = /^https?:\/\/archive\.(?:is|today)\//u;
+	f(prompt("URL:", web_reg.test(document.location) ? document.location : ""));
 }
