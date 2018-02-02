b4aeb96c30aba0342e2ca41643a2d94ae77ceea3
dou4cc <dou4cc@live.cn>
2018-02-02 17:36:48 +0800

    .

---
+++ ondestruct.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const guid = () => {
+	let d = Date.now();
+	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/x|y/gu, c => {
+		const r = Math.floor(d + Math.random() * 16) % 16;
+		d /= 16;
+		return (c === "x" ? r : r & 0x3 | 0x8).toString(16);
+	});
+};
+
+const db_name = () => "ondestruct/" + guid();
+
+indexedDB.open;
+
+const ref = new WeakMap;
+
+const ondestruct = (target, listener) => new Promise((resolve, reject) => {
+	const onfail = () => resolve(ondestruct(target, listener));
+	try{
+		const onopen = () => {
+			cn.result.close();
+			onfail();
+		};
+		new WeakSet([target]);
+		const cn = indexedDB.open(db_name());
+		cn.addEventListener("error", onfail);
+		cn.addEventListener("blocked", onfail);
+		cn.addEventListener("success", onopen);
+		cn.addEventListener("upgradeneeded", () => {
+			const ondelete = () => listener();
+			cn.removeEventListener("error", onfail);
+			cn.removeEventListener("success", onopen);
+			cn.result.createObjectStore("store", {keyPath: "key"});
+			const promise = new Promise(resolve => cn.addEventListener("success", resolve));
+			ref.set(target, cn.result);
+			const del = indexedDB.deleteDatabase(cn.result.name);
+			del.addEventListener("success", ondelete);
+			resolve(() => {
+				del.removeEventListener("success", ondelete);
+				promise.then(() => cn.result.close());
+			});
+		});
+	}catch(error){
+		if(!(error instanceof DOMError)) return reject(error);
+		onfail();
+	}
+});
+
+ondestruct;
