ee7ca544abcd5fb48fd226691bbe784f27ffaa7a
dou4cc <dou4cc@live.cn>
2018-02-03 17:32:39 +0800

    .

--- archiveis.js
+++ archiveis.js
@@ -1,15 +1,15 @@
-﻿//min
-javascript:((d,c,p,h,l=d.URL,b=/^https?:/.test(l),f=(r,t,u)=>{t!=u&&(u=d[c]("a"),u.href=t,d.head.appendChild(t=d[c]("base")).href=h,u=u.href,t.remove(),t=h+"archive.is/",u=u[p](/^https?:\/\/archive\.(is|today)\//,t),u==(u=u[p](r("^"+t+"o/(((?!_)\\w){4,}/)?"),""))?(r=r("^"+t),u=r.test(u)?u:t+"?run=1&url="+encodeURIComponent(h+"4r.gitlab.io/#"+u),!r.test(l)&b?open(u):location=u):f(r,u))})=>f(RegExp,prompt("URL:"+"\40".repeat(96),b?l:"")))(document,"createElement","replace","https://")
+//min
+javascript:((d,s,c,p,h,i,l=d.URL,w=l=="about:blank",b=/^https?:/.test(l),f=(r,t,u)=>t!=u?(u=d[c]("a"),u.href=t,d.head.appendChild(t=d[c]("base")).href=h,u=u.href,t.remove(),t=h+"archive.is/",u=u[p](/^https?:\/\/archive\.(is|today)\//,t),u==(u=u[p](r("^"+t+"o/(((?!_)\\w){4,}/)?"),""))?(r=r("^"+t),u=r.test(u)?u:t+"?run=1&url="+encodeURIComponent(h+"4r.gitlab.io/#"+u),!r.test(l)&b?open(u):location=u):f(r,u)):w&d.readyState[0]=="u"?location=l:0,g=_=>f(RegExp,prompt("URL:"+"\40".repeat(96),b?l:"")))=>{w&!d[i]?s(_=>s(g,d[i]=""),d[i]=0):g()})(document,setTimeout,"createElement","replace","https://","title")
 
 //source
 javascript:{
-	const f = url => {
-		if(url === null) return;
+	const parse = url => {
+		if(url === null) return document.location.href === "about:blank" && document.readyState === "uninitialized" && (document.location = "about:blank");
 		document.head.appendChild(base);
 		iframe.src = url;
 		url = iframe.src;
 		url = url.replace(archiveis_reg, archiveis_base);
-		if(url !== (url = url.replace(/^https:\/\/archive\.is\/o\/(?:[A-Za-z0-9]{4,}\/)?/u, ""))) return f(url);
+		if(url !== (url = url.replace(/^https:\/\/archive\.is\/o\/(?:[A-Za-z0-9]{4,}\/)?/u, ""))) return parse(url);
 		url =
 			archiveis_reg.test(url)
 			? url
@@ -20,6 +20,7 @@ javascript:{
 			document.location = url;
 		}
 	};
+	const start = () => parse(prompt("URL:" + " ".repeat(96), web_reg.test(document.location) ? document.location : ""));
 	const web_reg = /^https?:/u;
 	const base = document.createElement("base");
 	base.href = "https://";
@@ -27,6 +28,14 @@ javascript:{
 	const archiveis_reg = /^https?:\/\/archive\.(?:is|today)\//u;
 	const archiveis_base = "https://archive.is/";
 	const redirector = target => "https://4r.gitlab.io/#" + target;
-	f(prompt("URL:" + " ".repeat(96), web_reg.test(document.location) ? document.location : ""));
+	if(document.location.href === "about:blank" && document.title === ""){
+		document.title = "title";
+		setTimeout(() => {
+			document.title = "";
+			setTimeout(start);
+		});
+	}else{
+		start();
+	}
 	base.remove();
 }
