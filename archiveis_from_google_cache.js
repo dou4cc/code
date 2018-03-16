f867e7efedc8a633b86d23986cce0035824f929f
dou4cc <dou4cc@live.cn>
2018-03-16 17:46:19 +0800

    .

---
+++ archiveis_from_google_cache.js
@@ -0,0 +1,41 @@
+//min
+javascript:((d,s,c,p,h,i,l=d.URL,w=l=="about:blank",b=/^https?:/.test(l),f=(r,t,u)=>t!=u?(u=d[c]("a"),u.href=t,d.head.appendChild(t=d[c]("base")).href=h,u=u.href,t.remove(),t=h+"archive.is/",u=u[p](/^https?:\/\/archive\.(is|today)\//,t),u==(u=u[p](r("^"+t+"o/(((?!_)\\w){4,}/)?"),""))?(r=r("^"+t),u=r.test(u)?u:t+"?run=1&url="+encodeURIComponent(h+"4r.gitlab.io/#"+h+"webcache.googleusercontent.com/search?q=cache:"+u),!r.test(l)&b?open(u):location=u):f(r,u)):w&d.readyState[0]=="u"?location=l:0,g=_=>f(RegExp,prompt("URL:"+"\40".repeat(96),b?l:"")))=>{w&!d[i]?s(_=>s(g,d[i]=""),d[i]="Loading"):g()})(document,setTimeout,"createElement","replace","https://","title")
+
+//source
+javascript:{
+	const parse = url => {
+		if(url === null) return document.location.href === "about:blank" && document.readyState === "uninitialized" && (document.location = "about:blank");
+		document.head.appendChild(base);
+		iframe.src = url;
+		url = iframe.src;
+		url = url.replace(archiveis_reg, archiveis_base);
+		if(url !== (url = url.replace(/^https:\/\/archive\.is\/o\/(?:[A-Za-z0-9]{4,}\/)?/u, ""))) return parse(url);
+		url =
+			archiveis_reg.test(url)
+			? url
+			: archiveis_base + "?run=1&url=" + encodeURIComponent(redirector(url));
+		if(!archiveis_reg.test(document.location) && web_reg.test(document.location)){
+			open(url);
+		}else{
+			document.location = url;
+		}
+	};
+	const start = () => parse(prompt("URL:" + " ".repeat(96), web_reg.test(document.location) ? document.location : ""));
+	const web_reg = /^https?:/u;
+	const base = document.createElement("base");
+	base.href = "https://";
+	const iframe = document.createElement("iframe");
+	const archiveis_reg = /^https?:\/\/archive\.(?:is|today)\//u;
+	const archiveis_base = "https://archive.is/";
+	const redirector = target => "https://4r.gitlab.io/#https://webcache.googleusercontent.com/search?q=cache:" + target;
+	if(document.location.href === "about:blank" && document.title === ""){
+		document.title = "Loading";
+		setTimeout(() => {
+			document.title = "";
+			setTimeout(start);
+		});
+	}else{
+		start();
+	}
+	base.remove();
+}
